1b13d18c7ae3c675e98649852f0827ef6789f548
ASBP <s194582@student.dtu.dk>
2021-11-05 19:19:11 +0100

    main menu getting somewhere

--- app/src/main/java/com/example/nepalprojekt/MainActivity.kt
+++ app/src/main/java/com/example/nepalprojekt/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.nepalprojekt.ui.theme.NepalprojektTheme
 import kotlinx.coroutines.launch
+import javax.security.auth.Subject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,7 +40,154 @@ class MainActivity : ComponentActivity() {
 
                 }
             }*/
-            val scaffoldState = rememberScaffoldState()
+            Column (modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xffeba6f3))
+                //.background(Color(0xeba6f3))
+                //.fillMaxWidth()
+                //.border(5.dp, Color.Green)
+                //.padding(16.dp)
+                ,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+
+                MainMenu()
+                //Spacer(modifier = Modifier.height(50.dp))
+
+            }
+            /*//val painter = painterResource(id = R.drawable.kermit)
+            val description = "Kermit playing in the snow"
+            val title = "Kermit playing in the snow"
+            ImageCard(
+                //painter = painter,
+                contentDescription = description,
+                title = title
+            )*/
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Greeting("Android")
+}
+
+@Composable
+fun MainMenu(
+    modifier: Modifier = Modifier
+) {
+    MainMenuRow(title = "Health Info", subject1 = "Child", subject2 = "Parent")
+}
+
+@Composable
+fun MainMenuRow(
+    title: String,
+    subject1: String,
+    subject2: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        shape = RoundedCornerShape(10.dp), //rounded corners of outer box
+        modifier = Modifier
+            .padding(10.dp), //around outer box
+        elevation = 20.dp //still don't know what it does
+    ) {
+        Box (
+            modifier = Modifier
+                .background(Color.Green)
+                .padding(10.dp) //between inner and outer box
+        ){
+            Text(title,
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center),
+            modifier = Modifier.offset(10.dp, 0.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                MainMenuElement(
+                    subject = subject1
+                )
+                MainMenuElement(
+                    subject = subject2
+                )
+            }
+        }
+
+    }
+}
+
+@Composable
+fun MainMenuElement(
+    subject: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        elevation = 5.dp,
+        modifier = Modifier
+            .padding(5.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .clickable { }
+                .background(Color(0xFF101010))
+                .padding(10.dp)
+        ) {
+            Text(subject,
+                 style = TextStyle(
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center))
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    //painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(), //default
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            /*Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+                )*/
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
+        }
+    }
+}
+
+/* Textfields, Buttons and showing snackbars
+*
+* val scaffoldState = rememberScaffoldState()
             var textFieldState by remember {
                 mutableStateOf("")
             }
@@ -75,128 +223,4 @@ class MainActivity : ComponentActivity() {
                         Text("Pls greet me")
                     }
                 }
-            }
-            
-            Column (modifier = Modifier
-                .fillMaxSize()
-                //.fillMaxWidth()
-                //.border(5.dp, Color.Green)
-                //.padding(16.dp)
-                //.background(Color.Magenta),
-                ,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceEvenly
-            ) {
-
-                MainMenu()
-                //Spacer(modifier = Modifier.height(50.dp))
-
-            }
-            /*//val painter = painterResource(id = R.drawable.kermit)
-            val description = "Kermit playing in the snow"
-            val title = "Kermit playing in the snow"
-            ImageCard(
-                //painter = painter,
-                contentDescription = description,
-                title = title
-            )*/
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Greeting("Android")
-}
-
-@Composable
-fun MainMenu(
-    modifier: Modifier = Modifier,
-    //horizontalArrangement = Arrangement.CenterHorizontally,
-    //verticalArrangement = Arrangement.SpaceEvenly
-) {
-    Row (modifier = Modifier
-        .padding(20.dp)
-        .background(color = Color.Blue),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        MainMenuElement(
-            title = "Sundhedsinfo Barn"
-        )
-        Spacer(modifier = Modifier.width(50.dp))
-        MainMenuElement(
-            title = "Sundhedsinfo Forældre"
-        )
-    }
-    Row (modifier = Modifier
-        .padding(20.dp)
-        .background(color = Color.Blue),
-        horizontalArrangement = Arrangement.SpaceEvenly,
-        verticalAlignment = Alignment.CenterVertically,
-        ) {
-        MainMenuElement(
-            title = "Opskrifter Barn"
-        )
-        Spacer(modifier = Modifier.width(50.dp))
-        MainMenuElement(
-            title = "Opskrifter Forældre"
-        )
-    }
-}
-
-@Composable
-fun MainMenuElement(
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Box(
-        modifier = Modifier
-            .clickable { }
-            .background(Color(0xFF101010))
-            .padding(10.dp),
-        //shape = RoundedCornerShape(15.dp)
-    ) {
-        Text(title,
-             style = TextStyle(
-                color = Color.White,
-                fontSize = 16.sp,
-                textAlign = TextAlign.Center))
-    }
-}
-
-@Composable
-fun ImageCard(
-    //painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(), //default
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            /*Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-                )*/
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-            }
-        }
-    }
-}
+            }*/
