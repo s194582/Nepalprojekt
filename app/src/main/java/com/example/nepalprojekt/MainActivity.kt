0208c93156294dc48587d98dfd52f5e9de7448c6
ASBP <s194582@student.dtu.dk>
2022-01-03 14:29:53 +0100

    Reusable button works

--- app/src/main/java/com/example/nepalprojekt/MainActivity.kt
+++ app/src/main/java/com/example/nepalprojekt/MainActivity.kt
@@ -3,11 +3,7 @@ package com.example.nepalprojekt
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -16,9 +12,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.*
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -26,13 +19,8 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.NavHostController
 import com.example.nepalprojekt.ui.theme.Navigation
-import com.example.nepalprojekt.ui.theme.NepalprojektTheme
-import com.example.nepalprojekt.ui.theme.Recipes
 import com.example.nepalprojekt.ui.theme.Screen
-import kotlinx.coroutines.launch
-import javax.security.auth.Subject
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -102,14 +90,14 @@ fun MainMenu(
 
     ) {
 
-        MainMenuRow(title = "Health Info", subject1 = "Parent", subject2 = "Child", navController = navController)
-        MainMenuRow(title = "Recipes", subject1 = "Parent", subject2 = "Child", navController = navController)
+        HealthRow(title = "Health Info", subject1 = "Parent", subject2 = "Child", navController = navController)
+        RecipeRow(title = "Recipes", subject1 = "Parent", subject2 = "Child", navController = navController)
 
     }
 
 
 }
-
+/*
 @Composable
 fun MainMenuRow( // Grøn box
     navController: NavController,
@@ -191,6 +179,146 @@ fun MainMenuElement(
         }
     }
 }
+*/
+@Composable
+fun HealthRow( // Box containing two buttons for health info
+    navController: NavController,
+    title: String,
+    subject1: String,
+    subject2: String
+) {
+    Card( // for rounded shape
+        shape = RoundedCornerShape(10.dp), //rounded corners of outer box
+        modifier = Modifier
+            .padding(10.dp), //around outer box
+        elevation = 20.dp //still don't know what it does
+    ) {
+        Box (
+            modifier = Modifier
+                .background(Color.Green)
+                .padding(10.dp) //between inner and outer box
+        ){
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly) {
+                Text(title,
+                    style = TextStyle(
+                        color = Color.Black,
+                        fontSize = 18.sp,
+                        textAlign = TextAlign.Center)//.offset(40.dp, 40.dp))
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    ButtonElement(
+                        subject = subject1,
+                        navController = navController
+                    ) { navController.navigate(Screen.RecipeAdultScreen.route) } //<-route for onClick for button
+                    ButtonElement(
+                        subject = subject2,
+                        navController = navController
+                    ) { navController.navigate(Screen.RecipeAdultScreen.route) }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RecipeRow( // Box containing two buttons for recipes
+    navController: NavController,
+    title: String,
+    subject1: String,
+    subject2: String
+) {
+    Card( // for rounded shape
+        shape = RoundedCornerShape(10.dp), //rounded corners of outer box
+        modifier = Modifier
+            .padding(10.dp), //around outer box
+        elevation = 20.dp //still don't know what it does
+    ) {
+        Box (
+            modifier = Modifier
+                .background(Color.Green)
+                .padding(10.dp) //between inner and outer box
+        ){
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly) {
+                Text(title,
+                    style = TextStyle(
+                        color = Color.Black,
+                        fontSize = 18.sp,
+                        textAlign = TextAlign.Center)//.offset(40.dp, 40.dp))
+                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    ButtonElement(
+                        subject = subject1,
+                        navController = navController
+                    ) { navController.navigate(Screen.RecipeAdultScreen.route) }
+                    ButtonElement(
+                        subject = subject2,
+                        navController = navController
+                    ) { navController.navigate(Screen.RecipeAdultScreen.route) }
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Reusable button element
+ */
+@Composable
+fun ButtonElement(
+    navController: NavController,
+    subject: String,
+    onClick: () -> Unit
+) {
+    Card(
+        shape = RoundedCornerShape(10.dp),
+        elevation = 5.dp,
+        modifier = Modifier
+            .padding(5.dp)
+    ) {
+        Button(onClick = onClick //Creates reusable button
+            //modifier = Modifier
+            //.background(Color(0xFFb533b3))
+            //.padding(10.dp)
+        ) {
+            Text(subject,
+                style = TextStyle(
+                    color = Color.White,
+                    fontSize = 16.sp,
+                    textAlign = TextAlign.Center))
+        }
+
+
+        /*
+        Box(
+            modifier = Modifier
+                .background(Color(0xFFb533b3))
+                .padding(2.dp),
+            contentAlignment = Alignment.BottomStart
+        ) {
+            Text(subject, style = TextStyle(color = Color.Black, fontSize = 16.sp))
+        }
+
+         */
+    }
+}
+
+
 
 
 
