b3a60d396be3d15d4516a27c7066414f5281f32f
SAA-DK <sebastian_almfort@hotmail.com>
2022-01-06 10:44:50 +0100

    Debugger nu RecipeChildren.kt i forhold til at få den til at virke med MainActivity.kt

--- app/src/main/java/com/example/nepalprojekt/ui/theme/RecipeChildren.kt
+++ app/src/main/java/com/example/nepalprojekt/ui/theme/RecipeChildren.kt
@@ -1,2 +1,114 @@
 package com.example.nepalprojekt.ui.theme
 
+import android.icu.text.CaseMap
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.coerceAtLeast
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import org.w3c.dom.Text
+
+/**
+ * Inspiration for expanding info boxes from
+ * https://developer.android.com/codelabs/jetpack-compose-basics?hl=fr#0
+ */
+@Composable
+fun HealthChild (
+        navController: NavController,
+        titles: List<String> = List(1) {"$it"} //listOf("0-6", "6-9", "9-12")
+) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = titles) { title ->
+            InfoBoxes(title = "title1", description = ("Composem ipsum color sit lazy, " +
+                    "padding theme elit, sed do bouncy. ").repeat(4))
+            InfoBoxes(title = "title2", description = "description2")
+            InfoBoxes(title = "title3", description = "description3")
+        }
+    }
+}
+
+/**
+ * Each box containing health information
+ * In a Card shape, making rounded edges possible
+ */
+@Composable
+fun InfoBoxes (
+        title: String,
+        description: String
+) {
+    Card(
+            backgroundColor = MaterialTheme.colors.primary,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        InfoElement(title, description)
+    }
+}
+
+/**
+ * Modifications and animation for each health info element
+ */
+@Composable
+fun InfoElement (
+        title: String,
+        description: String
+) {
+    var expanded = rememberSaveable { mutableStateOf(false) } //rememberSaveable keeps expanded elements expanded
+    val extraPadding by animateDpAsState( //animates extraPadding, also animateDpAsState is interuptible
+            if (expanded.value) 48.dp else 0.dp,
+            animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+            )
+    )
+
+    Row (modifier = Modifier.padding(12.dp)
+
+    ) {
+        Column (modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding.coerceAtLeast(0.dp)
+                        //12.dp
+                )
+        ) {
+            Text(title) //Place of title of each element
+        }
+        OutlinedButton(
+                onClick = { expanded.value = !expanded.value }
+        ) {
+            Text(if (expanded.value) "Show less" else "Show more")
+        }
+        if (expanded.value) {
+            Text(description) //Place of description of each element
+        }
+
+        /* -----------For info content, instead of OutlinedButton
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+
+                )
+            }
+
+             */
+    }
