fb706fa866ef386dce142e2d61b4a0b1d0c0207b
SAA-DK <sebastian_almfort@hotmail.com>
2022-01-06 15:46:16 +0100

    Har omskrevet RecipeChildren.kt og prøver nu at debugge, ved at få Navigation.kt til at virke.

--- app/src/main/java/com/example/nepalprojekt/ui/theme/RecipeChildren.kt
+++ app/src/main/java/com/example/nepalprojekt/ui/theme/RecipeChildren.kt
@@ -7,9 +7,11 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -18,100 +20,60 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.nepalprojekt.ButtonElement
 import org.w3c.dom.Text
 
-/**
- * Inspiration for expanding info boxes from
- * https://developer.android.com/codelabs/jetpack-compose-basics?hl=fr#0
- */
 @Composable
-fun RecipeChild (
+fun RecipeChild( // Box containing two buttons for health info
         navController: NavController,
-        titles: List<String> = List(1) {"$it"} //listOf("0-6", "6-9", "9-12")
-) {
-    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
-        items(items = titles) { title ->
-            InfoBoxes(title = "title1", description = ("Composem ipsum color sit lazy, " +
-                    "padding theme elit, sed do bouncy. ").repeat(4))
-            InfoBoxes(title = "title2", description = "description2")
-            InfoBoxes(title = "title3", description = "description3")
-        }
-    }
-}
-
-/**
- * Each box containing health information
- * In a Card shape, making rounded edges possible
- */
-@Composable
-fun InfoBoxes (
         title: String,
-        description: String
+        subject1: String,
+        subject2: String
 ) {
-    Card(
-            backgroundColor = MaterialTheme.colors.primary,
-            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    Card( // for rounded shape
+            shape = RoundedCornerShape(10.dp), //rounded corners of outer box
+            modifier = Modifier
+                    .padding(10.dp), //around outer box
+            elevation = 20.dp //still don't know what it does
     ) {
-        InfoElement(title, description)
-    }
-}
-
-/**
- * Modifications and animation for each health info element
- */
-@Composable
-fun InfoElement (
-        title: String,
-        description: String
-) {
-    var expanded = rememberSaveable { mutableStateOf(false) } //rememberSaveable keeps expanded elements expanded
-    val extraPadding by animateDpAsState( //animates extraPadding, also animateDpAsState is interuptible
-            if (expanded.value) 48.dp else 0.dp,
-            animationSpec = spring(
-                    dampingRatio = Spring.DampingRatioMediumBouncy,
-                    stiffness = Spring.StiffnessLow
-            )
-    )
-
-    Row(modifier = Modifier.padding(12.dp)
-
-    ) {
-        Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding.coerceAtLeast(0.dp)
-                        //12.dp
+        Box (
+                modifier = Modifier
+                        .background(Color.Green)
+                        .padding(10.dp) //between inner and outer box
+        ){
+            Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceEvenly) {
+                Text(title,
+                        style = TextStyle(
+                                color = Color.Black,
+                                fontSize = 18.sp,
+                                textAlign = TextAlign.Center)//.offset(40.dp, 40.dp))
                 )
-        ) {
-            Text(title) //Place of title of each element
-        }
-        OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
-        ) {
-            Text(if (expanded.value) "Show less" else "Show more")
-        }
-        if (expanded.value) {
-            Text(description) //Place of description of each element
-        }
-
-        /* -----------For info content, instead of OutlinedButton
-            IconButton(onClick = { expanded = !expanded }) {
-                Icon(
-                    imageVector = if (expanded) Filled.ExpandLess else Filled.ExpandMore,
-                    contentDescription = if (expanded) {
-                        stringResource(R.string.show_less)
-                    } else {
-                        stringResource(R.string.show_more)
-                    }
-
-                )
+                Spacer(modifier = Modifier.height(10.dp))
+                Row(
+                        modifier = Modifier
+                                .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly,
+                        verticalAlignment = Alignment.CenterVertically
+                ) {
+                    ButtonElement(
+                            subject = subject1
+                    ) { navController.navigate(Screen.RecipeAdultScreen.route) } //<-route for onClick for button
+                    ButtonElement(
+                            subject = subject2
+                    ) { navController.navigate(Screen.RecipeChildren.route) }
+                    //{ navController.navigate(Screen.RecipeChildren.route) }
+                }
             }
-
-             */
+        }
     }
 }
