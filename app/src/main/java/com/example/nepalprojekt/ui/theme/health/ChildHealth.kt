e9289f5e9b8a2bab4cc803e64aaa86ea14450062
ASBP <s194582@student.dtu.dk>
2022-01-06 15:46:49 +0100

    Images added for health 0-6 month olds in ChildHealth.kt. Not connected to rest of app ATM

--- app/src/main/java/com/example/nepalprojekt/ui/theme/health/ChildHealth.kt
+++ app/src/main/java/com/example/nepalprojekt/ui/theme/health/ChildHealth.kt
@@ -1,5 +1,6 @@
 package com.example.nepalprojekt.ui.theme.health
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
@@ -7,6 +8,8 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,19 +26,39 @@ fun ZeroSix () {
         //.weight(1f)
         .padding(12.dp)
     ) {
+            // Picture of healthpost
+            Image(painter = painterResource(id = R.drawable.hc1_go_to_healthpost),
+                contentDescription = stringResource(id = R.string.zero_six_the_child_should_be_examined),
+                //contentScale = ContentScale.Crop
+            )
             // The child should be examined at the health institution on the third day within 24 hours of birth and at 4 months.
-        Text(stringResource(id = R.string.zero_six_the_child_should_be_examined))
-            // OBS! Blue on the spreadsheet
+            Text(stringResource(id = R.string.zero_six_the_child_should_be_examined))
+
+
+            // OBS! Blue on the spreadsheet, might mean picture is missing
             // The first yellow milk of the mother should be fed as soon as possible (within 1 hour) after the birth of the newborn baby.
         Text(stringResource(id = R.string.zero_six_the_first_yellow_milk))
+
+            Image(painter = painterResource(id = R.drawable.hc3_0_6month),
+                contentDescription = "",
+                //contentScale = ContentScale.Crop
+            )
             // From birth to 6 months the baby does not need to be fed water as it is exclusively breast milk.
-        Text(stringResource(id = R.string.zero_six_from_birth_to_6_months))
+            Text(stringResource(id = R.string.zero_six_from_birth_to_6_months))
+
+            Image(painter = painterResource(id = R.drawable.hc4_breastfeed6months_shortversion),
+                contentDescription = "",
+                //contentScale = ContentScale.Crop
+            )
             // Starting breastfeeding as soon as you are born helps to keep the milk constant.
-        Text(stringResource(id = R.string.zero_six_starting_breastfeeding_as_soon))
+            Text(stringResource(id = R.string.zero_six_starting_breastfeeding_as_soon))
+
             // Newborns should be breastfed at least 10 times a day, day and night.
         Text(stringResource(id = R.string.zero_six_newborns_should_be_breastfeed))
+
             // Feed from one breast only after feeding in another way.
         Text(stringResource(id = R.string.zero_six_feed_from_one_breast_only))
+
             // OBS! Might get deleted
             // Feeding the child 6 months of age and additional vitamin A capsules and parasite medicine every 6-6 months.
         Text(stringResource(id = R.string.zero_six_feeding_the_child_6_months))
