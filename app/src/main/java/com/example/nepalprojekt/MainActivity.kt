672d07cff14d854350128d86fa7e57486faa7db6
SAA-DK <sebastian_almfort@hotmail.com>
2022-01-06 11:13:42 +0100

    Prøvet et rydde på i MainActivity.kt

--- app/src/main/java/com/example/nepalprojekt/MainActivity.kt
+++ app/src/main/java/com/example/nepalprojekt/MainActivity.kt
@@ -26,27 +26,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            /*NepalprojektTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting(name = "Android")
 
-                }
-            }*/
             Navigation()
 
             val painter = painterResource(id = R.drawable.background1) // For designed background
 
-            //MainMenu(painter = painter)
-
-            /*//val painter = painterResource(id = R.drawable.)
-            val description = ""
-            val title = ""
-            ImageCard(
-                //painter = painter,
-                contentDescription = description,
-                title = title
-            )*/
         }
     }
 }
@@ -68,21 +52,11 @@ fun MainMenu(
         //painter: Painter,
         modifier: Modifier = Modifier
 ) {
-    /*var text by remember {
-        mutableStateOf("")
-    }
-    TextField(value = text, onValueChange = {text = it})
-    Button(onClick = { navController.navigate(Screen.RecipeAdultScreen.route) }) {
-        Text(text = "Test going to RecipeScreen")
-    }*/
+
     Box(
             modifier = Modifier.fillMaxSize()
     ){
-        /*Image(
-            painter = painter,
-            contentDescription = "backgroundPhoto",
-            contentScale = ContentScale.FillBounds
-        )*/ }
+         }
     Column (modifier = Modifier
             .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -93,59 +67,8 @@ fun MainMenu(
         RecipeRow(title = "Recipes", subject1 = "Parent", subject2 = "Child", navController = navController)
 
     }
-
-
 }
-/*
-@Composable
-fun MainMenuRow( // Grøn box
-    navController: NavController,
-    title: String,
-    subject1: String,
-    subject2: String,
-    //modifier: Modifier = Modifier
-) {
-    Card(
-        shape = RoundedCornerShape(10.dp), //rounded corners of outer box
-        modifier = Modifier
-            .padding(10.dp), //around outer box
-        elevation = 20.dp //still don't know what it does
-    ) {
-        Box (
-            modifier = Modifier
-                .background(Color.Green)
-                .padding(10.dp) //between inner and outer box
-        ){
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceEvenly) {
-                Text(title,
-                    style = TextStyle(
-                        color = Color.Black,
-                        fontSize = 18.sp,
-                        textAlign = TextAlign.Center)//.offset(40.dp, 40.dp))
-                )
-                Spacer(modifier = Modifier.height(10.dp))
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    MainMenuElement(
-                        subject = subject1,
-                        navController = navController
-                    )
-                    MainMenuElement(
-                        subject = subject2,
-                        navController = navController
-                    )
-                }
-            }
-        }
-    }
-}
-*/
+
 /**
  * Health info
  */
@@ -277,74 +200,3 @@ fun ButtonElement(
 
 
 
-/* Below is exercise code, used as inspiration
-*
-@Composable
-fun ImageCard(
-    //painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier.fillMaxWidth(), //default
-        shape = RoundedCornerShape(15.dp),
-        elevation = 5.dp
-    ) {
-        Box(modifier = Modifier.height(200.dp)) {
-            /*Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-                )*/
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
-            ) {
-                Text(title, style = TextStyle(color = Color.White, fontSize = 16.sp))
-            }
-        }
-    }
-}
-*/
-/* Textfields, Buttons and showing snackbars
-*
-* val scaffoldState = rememberScaffoldState()
-            var textFieldState by remember {
-                mutableStateOf("")
-            }
-            val scope = rememberCoroutineScope()
-
-            Scaffold(modifier = Modifier.fillMaxSize(),
-                scaffoldState = scaffoldState
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(horizontal = 30.dp)
-                ) {
-                    TextField(
-                        value = textFieldState,
-                        label = {
-                            Text("Enter your name")
-                        },
-                        onValueChange = { //called whenever the user changes something
-                            textFieldState = it
-                        },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth()
-                    )
-                    Spacer(modifier = Modifier.height(16.dp))
-                    Button(onClick = {
-                        scope.launch {
-                            scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState")
-                        }
-                    }) {
-                        Text("Pls greet me")
-                    }
-                }
-            }*/
