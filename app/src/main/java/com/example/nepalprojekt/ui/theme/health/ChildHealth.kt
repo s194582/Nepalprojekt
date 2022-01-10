bc5ef9822d40c244db5eb29f5d16eeade71ec64e
ASBP <s194582@student.dtu.dk>
2022-01-10 13:16:39 +0100

    six to nine months added. Rest health for children are waiting for finished resources from project provider.

--- app/src/main/java/com/example/nepalprojekt/ui/theme/health/ChildHealth.kt
+++ app/src/main/java/com/example/nepalprojekt/ui/theme/health/ChildHealth.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
@@ -20,7 +21,7 @@ import com.example.nepalprojekt.R
 
 val spaceBetweenChildren = 12.dp
 
-@Preview
+//@Preview
 @Composable
 fun ZeroSix () {
     Card(
@@ -61,14 +62,14 @@ fun ZeroSix () {
             // Feed from one breast only after feeding in another way.
             Text(stringResource(id = R.string.zero_six_feed_from_one_breast_only))
 
-            /** TO DO
-             * Revisit, as the following might get left out */
+            /* TODO: Revisit, as the following might get left out */
             // Feeding the child 6 months of age and additional vitamin A capsules and parasite medicine every 6-6 months.
             Text(stringResource(id = R.string.zero_six_feeding_the_child_6_months))
         }
     }
 }
 
+@Preview
 @Composable
 fun SixNine () {
     Card(
@@ -82,14 +83,93 @@ fun SixNine () {
             modifier = Modifier
                 .padding(12.dp)
         ) {
-            // Picture of
-            //Image(painter = painterResource(id = R.drawable.),
-            //    contentDescription = ""
-            //)
-            //
-            //Text(stringResource(id = R.string.))
+            // Picture of health post, same as 0-6
+            Image(painter = painterResource(id = R.drawable.hc1_go_to_healthpost),
+                contentDescription = "healthpost")
+            // After the child reaches 6 months and 9 months, go to the health institution for examination.
+            Text(stringResource(id = R.string.six_nine_after_the_child_reaches))
 
+            Image(painter = painterResource(id = R.drawable.hc21_6_9mdr_breastfeedingandfeeding_logo),
+                contentDescription = "breastfeeding and feeding")
+            // After the completion of 6 months (180 days), breast milk should be fed with different foods till the completion of two years.
+            Text(stringResource(id = R.string.six_nine_after_the_completion))
 
+            Image(painter = painterResource(id = R.drawable.hc22_6_9mdr_feedingandfood),
+                contentDescription = "feeding and food")
+            // When starting different feeding, feed 2 to 3 tablespoons at a time and then increase to 1 cup of tea (125 ml) at a time.
+            Text(stringResource(id = R.string.six_nine_when_starting_different))
+
+            Image(painter = painterResource(id = R.drawable.hc23_jauloandlitto),
+                contentDescription = "jaulo and litto")
+            // Different foods should be prepared daily by mixing at least 4 groups of food (grains, nuts, animal foods, fruits and vegetables).
+            Text(stringResource(id = R.string.six_nine_different_foods_should))
+            /*TODO
+            *  Consider adding link to recipes*/
+
+            Image(painter = painterResource(id = R.drawable.hc24_boiled_water),
+                contentDescription = "boiling water")
+            // Always use clean or boiled water.
+            Text(stringResource(id = R.string.six_nine_always_use_boiled))
+
+            Image(painter = painterResource(id = R.drawable.hc241_handwashing),
+                contentDescription = "wash hands")
+            // Wash hands thoroughly with soap and water before cooking and after feeding.
+            Text(stringResource(id = R.string.six_nine_wash_hands_thoroughly))
+
+            Image(painter = painterResource(id = R.drawable.hc25_two_children_logo_salt),
+                contentDescription = "salt logo")
+            // Only salt with two children logo should be used when preparing food.
+            Text(stringResource(id = R.string.six_nine_only_salt))
+        }
+    }
+}
+
+/* TODO: Revisit for changes in translations */
+@Composable
+fun NineTwelve () {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(spaceBetweenChildren),
+            modifier = Modifier
+                .padding(12.dp)
+        ) {
+            /* Picture of
+            Image(painter = painterResource(id = R.drawable.),
+                contentDescription = ""
+            )
+            Text(stringResource(id = R.string.))
+
+             */
+        }
+    }
+}
+
+/* TODO: Revisit for changes in translations */
+@Composable
+fun TwelveTwentyfour () {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier
+            .padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(spaceBetweenChildren),
+            modifier = Modifier
+                .padding(12.dp)
+        ) {
+            /* Picture of
+            Image(painter = painterResource(id = R.drawable.),
+                contentDescription = ""
+            )
+            Text(stringResource(id = R.string.))
+
+             */
         }
     }
 }
