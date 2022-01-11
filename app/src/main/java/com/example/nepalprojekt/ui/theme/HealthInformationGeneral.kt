14c4024117cae20aaecd34c20d1860bd2a813195
ASBP <s194582@student.dtu.dk>
2022-01-11 15:53:25 +0100

    Pregnancy information and images added to HealthInformationGeneral.kt

--- app/src/main/java/com/example/nepalprojekt/ui/theme/HealthInformationGeneral.kt
+++ app/src/main/java/com/example/nepalprojekt/ui/theme/HealthInformationGeneral.kt
@@ -3,6 +3,7 @@ package com.example.nepalprojekt.ui.theme
 import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,7 +12,10 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandLess
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
@@ -24,14 +28,38 @@ import com.example.nepalprojekt.R
 @Composable
 fun HealthGeneral (
     navController: NavController,
-    titles: List<String> = List(1) {"$it"} //listOf("0-6", "6-9", "9-12")
+    titles: List<String> = List(1) {"$it"}
 ) {
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         items(items = titles) { title ->
-            InfoBoxesExpandable(title = stringResource(id = R.string.pregnant_woman), description = null, descr2 = "Pregnant")
-            //InfoBoxesExpandable(title = stringResource(id = R.string.general_information), description = "General")
 
+            InfoBoxesExpandable(title = stringResource(id = R.string.pregnancy), // Pregnancy
+                painterResource(id = R.drawable.hp1_gravid_lyttermave),
+                null,
+                painterResource(id = R.drawable.hp2_gravid_vaccination),
+                stringResource(id = R.string.pregnancy_regular_health_check_ups),
+                painterResource(id = R.drawable.hp3_gravid_orm),
+                stringResource(id = R.string.pregnancy_after_3_months),
+                painterResource(id = R.drawable.hp4_balanceddiet_withmeat),
+                stringResource(id = R.string.pregnancy_eat_at_least),
+                null, // TODO: revisit to see if drawing has been made
+                stringResource(id = R.string.pregnancy_from_the_first),
+                null, // TODO: revisit to see if drawing has been made
+                stringResource(id = R.string.pregnancy_get_enough_rest))
 
+            InfoBoxesExpandable(title = stringResource(id = R.string.general_information),
+                null,
+                description = "General",
+                null,
+                stringResource(id = R.string.test),
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null,
+                null)
         }
     }
 }
@@ -43,14 +71,24 @@ fun HealthGeneral (
 @Composable
 fun InfoBoxesExpandable (
     title: String,
+    image: Painter?,
     description: String?,
-    descr2: String?
+    image2: Painter?,
+    desc2: String?,
+    image3: Painter?,
+    desc3: String?,
+    image4: Painter?,
+    desc4: String?,
+    image5: Painter?,
+    desc5: String?,
+    image6: Painter?,
+    desc6: String?
 ) {
     Card(
         backgroundColor = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        InfoElementExpandable(title, description, descr2)
+        InfoElementExpandable(title, image, description, image2, desc2, image3, desc3, image4, desc4, image5, desc5, image6, desc6)
     }
 }
 
@@ -60,10 +98,19 @@ fun InfoBoxesExpandable (
 
 @Composable
 fun InfoElementExpandable(
-
     title: String,
+    image: Painter?,
     description: String?,
-    descr2: String?
+    image2: Painter?,
+    desc2: String?,
+    image3: Painter?,
+    desc3: String?,
+    image4: Painter?,
+    desc4: String?,
+    image5: Painter?,
+    desc5: String?,
+    image6: Painter?,
+    desc6: String?
 ) {
     var expanded by remember { mutableStateOf(false) } //rememberSaveable keeps expanded elements expanded
 
@@ -85,18 +132,45 @@ fun InfoElementExpandable(
             )
         )
     ) {
-        Column (modifier = Modifier
-            .weight(1f)
-            .padding(12.dp)
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(spaceBetweenChildren),
+            modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)
         ) {
             Text(title) //Place of title of each element
 
             if (expanded) {
+                if (image != null) {
+                    Image(painter = image, contentDescription = null)
+                }
                 if (description != null) {
                     Text(description)
-                } //Place of description of each element
-                if (descr2 != null) {
-                    Text(descr2)
+                }
+                if (image2 != null) {
+                    Image(painter = image2, contentDescription = null)
+                }
+                if (desc2 != null) {
+                    Text(desc2)
+                }
+                if (image3 != null) {
+                    Image(painter = image3, contentDescription = null)
+                }
+                if (desc3 != null) {
+                    Text(desc3)
+                }
+                if (image4 != null) {
+                    Image(painter = image4, contentDescription = null)
+                }
+                if (desc4 != null) {
+                    Text(desc4)
+                }
+                if (image5 != null) {
+                    Image(painter = image5, contentDescription = null)
+                }
+                if (desc5 != null) {
+                    Text(desc5)
                 }
             }
 
---
+++ app/src/main/java/com/example/nepalprojekt/ui/theme/recipes/RecipesChild.kt
@@ -0,0 +1,113 @@
+package com.example.nepalprojekt.ui.theme.recipes
+
+import android.icu.text.CaseMap
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.coerceAtLeast
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.example.nepalprojekt.R
+import com.example.nepalprojekt.ui.theme.health.SixNine
+import com.example.nepalprojekt.ui.theme.health.ZeroSix
+import org.w3c.dom.Text
+
+/**
+ * Inspiration for expanding info boxes from
+ * https://developer.android.com/codelabs/jetpack-compose-basics?hl=fr#0
+ */
+@Composable
+fun RecipesChild (
+    navController: NavController,
+    titles: List<String> = List(1) {"$it"} //listOf("0-6", "6-9", "9-12")
+) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = titles) { title ->
+            InfoBoxesExpandable(title = stringResource(id = R.string.pregnant_woman), description = "Pregnant")
+            InfoBoxesExpandable(title = stringResource(id = R.string.general_information), description = "General")
+        }
+    }
+}
+
+/**
+ * Each box containing recipe information
+ * In a Card shape, making rounded edges possible
+ */
+@Composable
+fun InfoBoxesExpandable (
+    title: String,
+    description: String
+) {
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        InfoElementExpandable(title, description)
+    }
+}
+
+/**
+ * Modifications and animation for each recipe element
+ */
+@Composable
+fun InfoElementExpandable(
+    title: String,
+    description: String
+) {
+    var expanded by remember { mutableStateOf(false) } //rememberSaveable keeps expanded elements expanded
+
+    Row (modifier = Modifier
+        .padding(12.dp)
+        .animateContentSize( //animateContentSize automates the process of creating the animation (replaces extraPadding)
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )
+    ) {
+        Column (modifier = Modifier
+            .weight(1f)
+            .padding(12.dp)
+        ) {
+            Text(title) //Place of title of each element
+
+            if (expanded) {
+                Text(description) //Place of description of each element
+                Text("bh")
+
+            }
+        }
+
+        /** IconButton is represented as a small arrow pointing up or down */
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+
+                contentDescription = (if (expanded) { //String resources needed to make Icon work. Isn't shown in app.
+                    stringResource(R.string.show_less)
+                } else {
+                    stringResource(R.string.show_more)
+                })
+            )
+        }
+    }
+}
